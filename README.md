b1198917b528acd69b4b6139bee6c7bad5d2bbf4
evenyao <invictus@vip.qq.com>
2018-09-13 16:13:19 +0800

    add header

--- README.md
+++ README.md
@@ -4,18 +4,11 @@
 
 ## Build Setup
 
-``` bash
-# install dependencies
-npm install
 
-# serve with hot reload at localhost:8080
-npm run dev
+## 项目相关 npm 依赖包
+- fastClick
+- stylus
+- stylus-loader
 
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+## 设置样式变量
+通过 variable.styl 设置样式变量，抽离出公用样式。以方便维护
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont icon-fanhui"></div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont icon-sousuo"></span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont icon-jiantou"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  // 调用公用样式变量
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .icon-fanhui
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .icon-jiantou
+        margin-left: -.04rem
+        font-size: .24rem
+</style>

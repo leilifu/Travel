07355d5eb573ed9dd3a5acd0f148218c4279c44e
evenyao <invictus@vip.qq.com>
2018-09-13 17:22:28 +0800

    change

--- README.md
+++ README.md
@@ -12,3 +12,9 @@
 
 ## 设置样式变量
 通过 variable.styl 设置样式变量，抽离出公用样式。以方便维护
+
+## 使用 vue-awesome-swiper 轮播插件
+使用 2.6.7 版本
+```
+npm install vue-awesome-swiper@2.6.7 --save
+```
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+       <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+     <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function(){
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg"
+      }, {
+        id: '0002',
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1808/8a/1957a0fb58ad0402.jpg_750x200_e72b8c0f.jpg"
+      }, {
+        id: '0003',
+        imgUrl: "https://img1.qunarzz.com/piao/fusion/1808/57/9260f604f7215b02.jpg_750x200_377ef3f0.jpg"
+      }, {
+        id: '0004',
+        imgUrl: "http://img1.qunarzz.com/piao/fusion/1809/2d/0d7dc3400a50b502.jpg_750x200_4ca95dd4.jpg"
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  // 样式穿透
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 27%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
